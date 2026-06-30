cc4220e489fbe245454cdf6db3ce62a01ce30cd2
SHAYAN ROY <shayanroy74@gmail.com>
2026-06-30 22:12:35 +0530

    Time: 2 ms (72.75%), Space: 47.7 MB (56.34%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -9,6 +9,7 @@ class Solution {
             dfs(board, visited, x - 1, y);
             dfs(board, visited, x, y + 1);
             dfs(board, visited, x, y - 1);
+            
         }
     }
 
@@ -22,28 +23,30 @@ class Solution {
             for (int j = 0; j < board[0].length; j++) {
 
                 if (i == 0 || i == board.length - 1) {
-                    if (board[i][j] == 'O') {
-                        list.add(new int[] { i, j });
+                    if (board[i][j] == 'O' && !visited[i][j]) {
+                        //list.add(new int[] { i, j });
+                         dfs(board, visited, i, j);
                     }
 
                 } else {
                     if (j == 0 || j == board[0].length - 1) {
-                        if (board[i][j] == 'O') {
-                            list.add(new int[] { i, j });
+                        if (board[i][j] == 'O' && !visited[i][j]) {
+                            //list.add(new int[] { i, j });
+                            dfs(board, visited, i, j);
                         }
                     }
                 }
             }
         }
 
-        for (int i = 0; i < list.size(); i++) {
+        /*for (int i = 0; i < list.size(); i++) {
             int x = list.get(i)[0];
             int y = list.get(i)[1];
             if (!visited[x][y]) {
                 dfs(board, visited, x, y);
 
             }
-        }
+        }*/
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
