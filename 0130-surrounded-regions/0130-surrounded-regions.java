9465bab93fece9382f353249624c12f5d4dd19c2
SHAYAN ROY <shayanroy74@gmail.com>
2026-06-30 22:02:31 +0530

    Time: 3 ms (28.26%), Space: 47.8 MB (37.9%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,57 @@
+class Solution {
+    public void dfs(char[][] board, boolean[][] visited, int x, int y) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) {
+            return;
+        }
+        if (!visited[x][y] && board[x][y] == 'O') {
+            visited[x][y] = true;
+            dfs(board, visited, x + 1, y);
+            dfs(board, visited, x - 1, y);
+            dfs(board, visited, x, y + 1);
+            dfs(board, visited, x, y - 1);
+        }
+    }
+
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+
+        ArrayList<int[]> list = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+
+                if (i == 0 || i == board.length - 1) {
+                    if (board[i][j] == 'O') {
+                        list.add(new int[] { i, j });
+                    }
+
+                } else {
+                    if (j == 0 || j == board[0].length - 1) {
+                        if (board[i][j] == 'O') {
+                            list.add(new int[] { i, j });
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            int x = list.get(i)[0];
+            int y = list.get(i)[1];
+            if (!visited[x][y]) {
+                dfs(board, visited, x, y);
+
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'O' && !visited[i][j]) {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+
+    }
+}
